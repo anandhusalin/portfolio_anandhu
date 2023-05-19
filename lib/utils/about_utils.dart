852cdb5c945aabffff1ceba69813be9903ce81bc
anandhusalin <anandhusalin1997@gmail.com>
2023-05-20 00:28:59 +0530

    first commit

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Anandhu Salin, a Flutter developer';
+
+  static const String aboutMeDetail =
+      'I\'m a Fresh Graduate with Computer Science as Majors from KTU University, kerala. I have been developing mobile apps for over One and half years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience.Currently working as Associate Software Engineer at butomy';
+}
